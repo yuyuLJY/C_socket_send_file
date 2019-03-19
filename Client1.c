b0ec763f857560a9134ccc886fb09ffb5732a44e
yuyuLJY <jiayu1922@163.com>
2019-03-19 18:27:46 +0800

    Update Client1.c

--- Client1.c
+++ Client1.c
@@ -15,10 +15,8 @@ int main(){
     struct hostent *h;
     struct sockaddr_in  client_addr;
     h=gethostbyname(IP);
-    //写出linux端的IP和端口
-    //Linux端的IP号：192.168.126.130
-    //调用WSAStartup初始化套接字
 
+    //调用WSAStartup初始化套接字
     WSADATA WSAData;
     if (WSAStartup(MAKEWORD(2,0), &WSAData) != 0)
     {
@@ -33,34 +31,36 @@ int main(){
 	}
 	printf("创建socket成功\n");
 	printf("IP:%s\n",IP);
+	//写出linux端的IP和端口
+        //Linux端的IP号：192.168.126.130
 	memset(&client_addr, 0, sizeof(client_addr));
 	client_addr.sin_family = AF_INET;
 	client_addr.sin_port = htons(portnum);
-    client_addr.sin_addr.s_addr = inet_addr(IP);
+    	client_addr.sin_addr.s_addr = inet_addr(IP);
 
-    /*
+       /*
 	if( inet_pton(AF_INET, IP, &client_addr.sin_addr) <= 0){
         printf("端口和IP错误\n");
         exit(0);
 	}*/
+	//只有连接正确，才能够继续进行传输文件
 	if( connect(client_fd, (struct sockaddr*)&client_addr, sizeof(client_addr)) < 0){
         printf("连接错误\n");
         exit(0);
 	}
 
-    recievfile(client_fd); //向server发送文件
-    return 0;
+        recievfile(client_fd); //接收server的文件
+        return 0;
 
 }
 
 int recievfile(int client_fd){
-    // 输入文件名 并放到缓冲区buffer中等待发送
     char  *fname="C:\\Users\\yuyu\\Desktop\\新建文件夹\\网络安全\\write.txt";  //写进这个里边
     char buffer[BUFFER_SIZE];
     memset( buffer,0, sizeof(buffer) );
     strncpy(buffer, fname, strlen(fname)>sizeof(buffer)?sizeof(buffer):strlen(fname));
 
-     FILE *fp = fopen(fname,"w");
+     FILE *fp = fopen(fname,"w");//写文件，要记得关闭
      if( fp==NULL )
      {
          printf("File:\t%s Can Not Open To Write\n", fname);
@@ -86,7 +86,7 @@ int recievfile(int client_fd){
         memset( buffer,0, sizeof(buffer) );
     }
 
-	// 接收成功后，关闭文件，关闭socket
+     // 接收成功后，关闭文件，关闭socket
      printf("Receive File:\t%s From Server IP Successful!\n", fname);
      close(fp);
      close(client_fd);
